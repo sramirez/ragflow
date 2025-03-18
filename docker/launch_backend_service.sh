57068633a3ecc574de956bcf2ed8904ae72889a4
sramirez <sergio.ramirez@thalesaleniaspace.com>
2025-03-18 13:31:18 +0100

    Add Langfuse env to launch_backend_service.sh

--- docker/launch_backend_service.sh
+++ docker/launch_backend_service.sh
@@ -6,6 +6,10 @@ set -e
 # Unset HTTP proxies that might be set by Docker daemon
 export http_proxy=""; export https_proxy=""; export no_proxy=""; export HTTP_PROXY=""; export HTTPS_PROXY=""; export NO_PROXY=""
 export PYTHONPATH=$(pwd)
+export LANGFUSE_SECRET_KEY=#"SET"
+export LANGFUSE_PUBLIC_KEY=#"SET"
+# 🇪🇺 EU region
+export LANGFUSE_HOST="https://cloud.langfuse.com"
 
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
 JEMALLOC_PATH=$(pkg-config --variable=libdir jemalloc)/libjemalloc.so
